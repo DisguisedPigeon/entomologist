9154902b7c6cf2190d0f416f6c69a9a913420bcd
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2025-05-26 21:56:34 +0200

    :memo: Added docs on erlang-land too

--- src/entomologist/entomologist_logger_ffi.erl
+++ src/entomologist/entomologist_logger_ffi.erl
@@ -1,17 +1,26 @@
 -module(entomologist_logger_ffi).
 
 -moduledoc("A erlang logger custom handler.\n\nIt saves logs to a DB with "
-           "a specific schema.").
+           "a specific schema defined in github.com/DisguisedPigeon/entomologist"
+           "'s README.md").
 
 -export([log/2, configure/1]).
 
 -behaviour(logger_handler).
+
+-doc("internal configuration function for entomologist's the logger "
+     "handler.\n\nIt adds this module to the logger registered handlers "
+     "with a gleam `pog.Connection` value in the config.").
 
 configure(DbConnection) ->
         logger:add_handler(entomologist,
                            entomologist_logger_ffi,
                            #{config => #{connection => DbConnection}}),
         {ok, nil}.
+
+-doc("Saves a log message to DB using the gleam entomologist module.\n\nTh"
+     "is is a callback called by kernel:logger.\n\nIt casts the msg "
+     "field to a string in various manners if needed.").
 
 log(#{msg := {string, String},
       level := Level,
@@ -44,6 +53,10 @@ log(#{msg := Msg,
 log(_, _) ->
         error.
 
+-doc("Represents an arbitrary value as a json string.\n\n This is "
+     "used to save the metadata in gleam-land case it holds custom "
+     "fields").
+
 filter_parsed_and_to_json(Meta) ->
         iolist_to_binary(json:encode(
                                  maps:filter(fun(K, _) ->
@@ -56,6 +69,10 @@ filter_parsed_and_to_json(Meta) ->
                                                            line])
                                              end,
                                              Meta))).
+
+-doc("Represents an arbitrary value as a string.\n\n This is used "
+     "in case the log function is called with an unrecognized type "
+     "on the msg field.").
 
 binary_representation(Term) ->
         list_to_binary(io_lib:format("~p", [Term])).
